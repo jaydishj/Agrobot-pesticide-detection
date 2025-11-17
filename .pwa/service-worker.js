0e7d2841a0cfb5e758fc3382ceeb90d271bdbd89
JAYDISH KENNEDY J <jaydishkennedy@gmail.com>
2025-11-17 22:51:19 +0530

    Refactor service worker event listeners

--- .pwa/service-worker.js
+++ .pwa/service-worker.js
@@ -1,11 +1,5 @@
-self.addEventListener("install", function (event) {
-  console.log("Service Worker installed.");
-});
-
-self.addEventListener("activate", function (event) {
-  console.log("Service Worker activated.");
-});
-
-self.addEventListener("fetch", function (event) {
+self.addEventListener("install", () => {});
+self.addEventListener("activate", () => {});
+self.addEventListener("fetch", (event) => {
   event.respondWith(fetch(event.request));
 });
